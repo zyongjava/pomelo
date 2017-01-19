99489912326420183a6ba8c18e503486b7d1c4f1
zhengyong <yong.zheng@fraudmetrix.cn>
2017-01-19 14:01:18 +0800

    add spark master

--- src/main/scala/spark.scala
+++ src/main/scala/spark.scala
@@ -10,7 +10,7 @@ object spark {
 
   def main(args: Array[String]) {
     val logFile = "file:///Users/zhengyong/Development/spark-2.1.0-bin-hadoop2.7/README.md" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application")
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local");
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
