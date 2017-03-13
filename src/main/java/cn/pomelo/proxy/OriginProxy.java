6dd2c2cbd5ad4cdcb6942e54234c384252475c1a
zhengyong <yong.zheng@fraudmetrix.cn>
2017-03-13 17:25:06 +0800

    jdk代理修改

--- src/main/java/cn/pomelo/proxy/OriginProxy.java
+++ src/main/java/cn/pomelo/proxy/OriginProxy.java
@@ -5,33 +5,45 @@ package cn.pomelo.proxy;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 /**
- * 
- * 代理类实现InvocationHandler
+ * jdk代理 代理<br/>
+ *
+ * 类实现InvocationHandler
  * 
  * @author pomelo
- *
  */
-public class OriginProxy implements InvocationHandler{
-	
-	private Object target;
-	
-	public  OriginProxy(OriginInterface target){
-		super();
-		this.target = target;
-	}
+public class OriginProxy implements InvocationHandler {
 
-	@Override
-	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		
-		System.out.println("begin invoke method.");
-		
-		Object result = method.invoke(target, args);
-		
-		System.out.println("end invoke method.");
-		
-		return result;
-	}
+    private Object target;
+
+    public OriginProxy(OriginInterface target){
+        super();
+        this.target = target;
+
+    }
+
+    /**
+     * 构建代理对象
+     * 
+     * @return OriginInterface
+     */
+    public OriginInterface build() {
+        return (OriginInterface) Proxy.newProxyInstance(target.getClass().getClassLoader(),
+                                                        target.getClass().getInterfaces(), this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        System.out.println("begin invoke method.");
+
+        Object result = method.invoke(target, args);
+
+        System.out.println("end invoke method.");
+
+        return result;
+    }
 
 }
--- src/main/java/cn/pomelo/proxy/ProxyTest.java
+++ src/main/java/cn/pomelo/proxy/ProxyTest.java
@@ -3,21 +3,21 @@
  */
 package cn.pomelo.proxy;
 
-import java.lang.reflect.Proxy;
-
 /**
  * @author pomelo
- *
  */
 public class ProxyTest {
-	
-	public static void main(String[] args) {
-		// 实例化目标对象
-		OriginInterface target = new OriginImpl();
-		// 根据目标对象创建代理对象
-		OriginInterface proxy = (OriginInterface) Proxy.newProxyInstance(target.getClass().getClassLoader(), target.getClass().getInterfaces(), new OriginProxy(target));
-		// 调用代理对象方法
-		proxy.testProxy();
-	}
+
+    public static void main(String[] args) {
+        // 实例化目标对象
+        OriginInterface target = new OriginImpl();
+
+        // 根据目标对象创建代理对象
+        OriginProxy proxyObject = new OriginProxy(target);
+        OriginInterface proxy = proxyObject.build();
+
+        // 调用代理对象方法
+        proxy.testProxy();
+    }
 
 }
