1dc06212b919d5791ddd9d68013eee0b80930365
zhengyong <yong.zheng@tongdun.cn>
2018-08-09 14:52:29 +0800

    drools

--- src/main/java/drools/DroolsTest.java
+++ src/main/java/drools/DroolsTest.java
@@ -1,21 +1,25 @@
 package drools;
 
+import org.kie.api.KieBase;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
+import org.kie.internal.utils.KieHelper;
 
 import java.math.BigDecimal;
 
 /* This is a sample class to launch a rule. */
 
+/**
+ * 6.X       API  KIE
+ */
 public class DroolsTest {
 
     public static final void main(String[] args) {
 
-        KieServices kieServices = KieServices.Factory.get();
-        KieContainer kieContainer = kieServices.getKieClasspathContainer();
-
-        KieSession kieSession = kieContainer.newKieSession("ksession-rules");
+        KieSession kieSession = buildKieSession1();
+        KieSession kieSession2 = buildKieSession2();
 
         ItemCity item1 = new ItemCity();
         item1.setPurchaseCity(ItemCity.City.HANGZHOU);
@@ -23,6 +27,7 @@ public class DroolsTest {
         item1.setSellPrice(new BigDecimal(10));
         kieSession.insert(item1);
 
+        // 创建Fact对象
         ItemCity item2 = new ItemCity();
         item2.setPurchaseCity(ItemCity.City.HANGZHOU);
         item2.setTypeofItem(ItemCity.Type.TOURISM);
@@ -48,6 +53,35 @@ public class DroolsTest {
         System.out.println(item3.getPurchaseCity().toString() + ": " + item3.getLocalTax().intValue());
         System.out.println(item4.getPurchaseCity().toString() + ": " + item4.getLocalTax().intValue());
 
+        kieSession.dispose();
+
+    }
+
+    /**
+     * 方式一：创建KieSession
+     *
+     * @return KieSession
+     */
+    private static KieSession buildKieSession1() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+        // 依赖META-INF下kmodule.xml文件
+        KieSession kieSession = kieContainer.newKieSession("ksession-rules");
+        return kieSession;
+    }
+
+    /**
+     * 方式二：创建KieSession
+     *
+     * @return KieSession
+     */
+    private static KieSession buildKieSession2() {
+        KieHelper kieHelper = new KieHelper();
+        kieHelper.addResource(ResourceFactory.newClassPathResource("rules/chengdu.drl"));
+        kieHelper.addResource(ResourceFactory.newClassPathResource("rules/hangzhou.drl"));
+        KieBase kieBase = kieHelper.build();
+        KieSession kieSession = kieBase.newKieSession();
+        return kieSession;
     }
 
 }
